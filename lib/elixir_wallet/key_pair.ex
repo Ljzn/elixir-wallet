dc4f21d2456cf570ec00ae02e62e07b8747d7089
gspasov <georgi.spasov@quanterall.com>
2017-12-04 18:57:48 +0200

    [GH-#49] Finilizing the functionality, fixing old tests

--- lib/elixir_wallet/key_pair.ex
+++ lib/elixir_wallet/key_pair.ex
@@ -4,8 +4,8 @@ defmodule KeyPair do
   """
 
 
-  alias Structs.Bip32PubKey
-  alias Structs.Bip32PrivKey
+  alias Structs.Bip32PubKey, as: PubKey
+  alias Structs.Bip32PrivKey, as: PrivKey
 
 
   # Constant for generating the private_key / chain_code
@@ -17,65 +17,49 @@ defmodule KeyPair do
   # Mersenne number / TODO: type what it is used for
   @mersenne_prime 2147483647
 
-  @doc """
-  Generating a root seed from given mnemonic phrase
-  to further ensure uniqueness of master keys.
-  ## Example
-      iex> KeyPair.generate_root_seed("mnemonic", "pass")
-
-      %{"6C055755B1F6E97DFFC1C40C1BD4919C48938B211139C12C3F04A7F011D8DD20",
-      "03C6D13F979E118C97029A3F210AA207CA6695908BA814271472ED1775E4FFBC75",
-      <<18, 216, 49, 31, 0, 27, 92, 61, 81, 76, 17, 212, 106, 24, 176, 124, 144, 111,
-      182, 17, 157, 236, 54, 168, 91, 92, 99, 234, 76, 232, 20, 169>>
-      }
-  """
-  @spec generate_root_seed(String.t(), String.t(), List.t()) :: Map.t()
-  def generate_root_seed(mnemonic, password \\ "", opts \\ []) do
-    ## FIX
-  end
 
   def generate_seed(mnemonic, pass_phrase \\ "", opts \\ []) do
     SeedGenerator.generate(mnemonic, pass_phrase, opts)
   end
 
-  def generate_master_key(seed, :seed) do
-    generate_master_key(:crypto.hmac(:sha512, @bitcoin_const, seed), :private)
+  def generate_master_key(seed_bin, :seed) do
+    generate_master_key(:crypto.hmac(:sha512, @bitcoin_const, seed_bin), :private)
   end
 
   def generate_master_key(<<priv_key::binary-32, chain_code::binary>>, :private) do
-    key = Bip32PrivKey.create(:mainnet)
+    key = PrivKey.create(:mainnet)
     key = %{key | key: priv_key, chain_code: chain_code}
-
-    #KeyPair.derive(key, "m/0'/1/2'") |> KeyPair.generate_master_key(:public)
-    #KeyPair.format_key(KeyPair.derive(key, "m/0'/1/2'"))
   end
-  def generate_master_key(%Bip32PrivKey{} = priv_key, :public) do
+  def to_public_key(%PrivKey{} = priv_key) do
     pub_key = KeyPair.generate_pub_key(priv_key)
-    key = Bip32PubKey.create(:mainnet)
-    #IO.inspect "Private key chain"
-    #IO.inspect priv_key.chain_code
+    key = PubKey.create(:mainnet)
     key = %{key |
             depth: priv_key.depth,
             f_print: priv_key.f_print,
             child_num: priv_key.child_num,
             chain_code: priv_key.chain_code,
             key: pub_key}
-    #KeyPair.format_key(key)
   end
 
-  def generate_pub_key(%Bip32PrivKey{key: priv_key} = key) do
+  def generate_pub_key(%PrivKey{key: priv_key} = key) do
     {pub_key, _rest} = :crypto.generate_key(:ecdh, :secp256k1, priv_key)
     pub_key
   end
-  def generate_pub_key(%Bip32PrivKey{key: priv_key} = key, :compressed) do
-    key |> KeyPair.generate_pub_key() |> KeyPair.compress()
+  def generate_pub_key(%PrivKey{key: priv_key} = key, :compressed) do
+    key
+    |> KeyPair.generate_pub_key()
+    |> KeyPair.compress()
   end
 
-  def fingerprint(%Bip32PrivKey{key: priv_key} = key) do
-    KeyPair.fingerprint(KeyPair.generate_pub_key(key, :compressed))
+  def fingerprint(%PrivKey{key: priv_key} = key) do
+    key
+    |> KeyPair.generate_pub_key(:compressed)
+    |> KeyPair.fingerprint()
   end
-  def fingerprint(%Bip32PubKey{key: pub_key} = key) do
-    KeyPair.fingerprint(KeyPair.compress(pub_key))
+  def fingerprint(%PubKey{key: pub_key} = key) do
+    pub_key
+    |> KeyPair.compress()
+    |> KeyPair.fingerprint()
   end
   def fingerprint(pub_key) do
     <<f_print::binary-4, _rest::binary>> =
@@ -83,12 +67,12 @@ defmodule KeyPair do
     f_print
   end
 
-  defp serialize(%Bip32PubKey{key: pub_key} = key) do
+  defp serialize(%PubKey{key: pub_key} = key) do
     compressed_pub_key = KeyPair.compress(pub_key)
     {<<key.version::size(32)>>, <<key.depth::size(8), key.f_print::binary-4,
      key.child_num::size(32), key.chain_code::binary, compressed_pub_key::binary>>}
   end
-  defp serialize(%Bip32PrivKey{} = key) do
+  defp serialize(%PrivKey{} = key) do
     {<<key.version::size(32)>>, <<key.depth::size(8), key.f_print::binary-4,
      key.child_num::size(32), key.chain_code::binary, <<0::size(8)>>, key.key::binary>>}
   end
@@ -98,7 +82,13 @@ defmodule KeyPair do
     Base58Check.encode58check(prefix, data)
   end
 
-  def derive(key, <<"m/", path::binary>>) do
+  def derive(key, <<"m/", path::binary>>) do ## Deriving private keys
+    KeyPair.derive(key, path, :private)
+  end
+  def derive(key, <<"M/", path::binary>>) do ## Deriving public keys
+    derive(key, path, :public)
+  end
+  def derive(key, path, type) do
     KeyPair.derive_pathlist(
       key,
       :lists.map(fn(elem) ->
@@ -113,16 +103,21 @@ defmodule KeyPair do
             {num, _rest} = Integer.parse(elem)
             num
         end
-      end, :binary.split(path, <<"/">>, [:global])))
+      end, :binary.split(path, <<"/">>, [:global])),
+      type)
   end
 
-  def derive_pathlist(key, []), do: key
-  def derive_pathlist(key, pathlist) do
+  def derive_pathlist(key, [], :private), do: key
+  def derive_pathlist(key, [], :public), do: KeyPair.to_public_key(key)
+  def derive_pathlist(key, pathlist, type) do
     [index | rest] = pathlist
-    KeyPair.derive_pathlist(derive_key(key, index), rest)
+    key
+    |> derive_key(index)
+    |> KeyPair.derive_pathlist(rest, type)
   end
 
-  def derive_key(%Bip32PrivKey{depth: d} = key, index) when index <= @mersenne_prime do
+
+  def derive_key(%PrivKey{} = key, index) when index > -1 and index <= @mersenne_prime do
     # Normal derivation
     compressed_pub_key =
         KeyPair.generate_pub_key(key, :compressed)
@@ -137,7 +132,8 @@ defmodule KeyPair do
     KeyPair.derive_key(key, :binary.encode_unsigned(child_key), child_chain, index)
   end
 
-  def derive_key(%Bip32PrivKey{depth: d} = key, index) when index > @mersenne_prime do
+
+  def derive_key(%PrivKey{} = key, index) when index > @mersenne_prime do
     # Hardned derivation
     <<derived_key::size(256), child_chain::binary>> =
       :crypto.hmac(:sha512, key.chain_code,
@@ -148,60 +144,30 @@ defmodule KeyPair do
     KeyPair.derive_key(key, :binary.encode_unsigned(child_key), child_chain, index)
   end
 
-  def derive_key(%Bip32PubKey{depth: d} = key, index) when index <= @mersenne_prime do
+
+  def derive_key(%PubKey{} = key, index) when index > -1 and index <= @mersenne_prime do
     # Normal derivation
     serialized_pub_key = KeyPair.compress(key.key)
 
-    <<derived_key::256, child_chain::binary>> =
+    <<derived_key::size(256), child_chain::binary>> =
       :crypto.hmac(:sha512, key.chain_code,
         <<serialized_pub_key::binary, index::size(32)>>)
 
     {point, _} = :crypto.generate_key(:ecdh, :secp256k1, derived_key)
 
-    IO.inspect "#################  Parent key  ##################"
-    IO.inspect(key.key, limit: :infinity)
-    IO.inspect "#################  Point  ###################"
-    IO.inspect(point, limit: :infinity)
-    IO.inspect "#################  Derived Key   ###################"
-    IO.inspect(derived_key, limit: :infinity)
-
-
-    ## MAGIC HAPPENS HERE !!!!
-
-    <<_rest::8, x_par::256, y_par::256>> = key.key
-    <<_rest::8, x_tw::256, y_tw::256>> = point
-
-    p = 115792089237316195423570985008687907853269984665640564039457584007908834671663
-    h = (y_tw - y_par) / (x_tw - y_par)
-    IO.inspect round(h)
-    lambda = rem(h, p)
-
-    x_r = rem((lambda*lambda - x_tw - x_par), p)
-    y_r = rem((lambda*(x_tw - x_r) - y_tw), p)
-
-    IO.inspect "################## X_R ##################"
-    IO.inspect(x_r, limit: :infinity)
-    IO.inspect "################## X_R ##################"
-    IO.inspect(y_r, limit: :infinity)
-
-
-    IO.inspect <<4::8, x_r::size(256), y_r::size(256)>> |> KeyPair.compress()
-
-    <<point_int::520>> = point
-    <<parent_key_int::264>> = serialized_pub_key
+    <<point_int::size(520)>> = point
+    <<parent_key_int::size(264)>> = serialized_pub_key
 
     child_key = point_int + parent_key_int
-
-    l = :binary.encode_unsigned(child_key) |> KeyPair.compress()
-    IO.inspect "################## L ##################"
-    IO.inspect l
     KeyPair.derive_key(key, :binary.encode_unsigned(child_key), child_chain, index)
   end
 
-  def derive_key(%Bip32PubKey{depth: d} = key, index) when index > @mersenne_prime do
+
+  def derive_key(%PubKey{}, index) when index > @mersenne_prime do
     # Hardned derivation
-    throw("Cannot derive Public Hardened child")
+    raise(RuntimeError, "Cannot derive Public Hardened child")
   end
+
 
   def derive_key(key, child_key, child_chain, index) when is_map(key) do
     key = %{key |
@@ -220,7 +186,7 @@ defmodule KeyPair do
   """
   @spec generate_wallet_address(String.t()) :: String.t()
   def generate_wallet_address(public_key) do
-    public_sha256 = :crypto.hash(:sha256, Base.decode16!(public_key))
+    public_sha256 = :crypto.hash(:sha256, Base.decode16!(public_key, case: :mixed))
 
     public_ripemd160 = :crypto.hash(:ripemd160, public_sha256)
 
--- test/child_key_derivation.ex
+++ test/child_key_derivation.ex
@@ -9,7 +9,7 @@ defmodule ChildKeyDerivation do
 
     ## Chain m
     assert "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
-    = master_key |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    = master_key |> KeyPair.to_public_key() |> KeyPair.format_key()
     assert "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
     = KeyPair.format_key(master_key)
 
@@ -17,7 +17,7 @@ defmodule ChildKeyDerivation do
     ## Chain m/0h
     assert "xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw"
     = master_key
-    |> KeyPair.derive("m/0'") |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0'") |> KeyPair.format_key()
     assert "xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7"
     = master_key |> KeyPair.derive("m/0'") |> KeyPair.format_key()
 
@@ -25,7 +25,7 @@ defmodule ChildKeyDerivation do
     ## Chain m/0h/1
     assert "xpub6ASuArnXKPbfEwhqN6e3mwBcDTgzisQN1wXN9BJcM47sSikHjJf3UFHKkNAWbWMiGj7Wf5uMash7SyYq527Hqck2AxYysAA7xmALppuCkwQ"
     = master_key
-    |> KeyPair.derive("m/0'/1") |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0'/1") |> KeyPair.format_key()
     assert "xprv9wTYmMFdV23N2TdNG573QoEsfRrWKQgWeibmLntzniatZvR9BmLnvSxqu53Kw1UmYPxLgboyZQaXwTCg8MSY3H2EU4pWcQDnRnrVA1xe8fs"
     = master_key |> KeyPair.derive("m/0'/1") |> KeyPair.format_key()
 
@@ -33,7 +33,7 @@ defmodule ChildKeyDerivation do
     ## Chain m/0h/1/2h
     assert "xpub6D4BDPcP2GT577Vvch3R8wDkScZWzQzMMUm3PWbmWvVJrZwQY4VUNgqFJPMM3No2dFDFGTsxxpG5uJh7n7epu4trkrX7x7DogT5Uv6fcLW5"
     = master_key
-    |> KeyPair.derive("m/0'/1/2'") |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0'/1/2'") |> KeyPair.format_key()
     assert "xprv9z4pot5VBttmtdRTWfWQmoH1taj2axGVzFqSb8C9xaxKymcFzXBDptWmT7FwuEzG3ryjH4ktypQSAewRiNMjANTtpgP4mLTj34bhnZX7UiM"
     = master_key |> KeyPair.derive("m/0'/1/2'") |> KeyPair.format_key()
 
@@ -41,7 +41,7 @@ defmodule ChildKeyDerivation do
     ## Chain m/0h/1/2h/2
     assert "xpub6FHa3pjLCk84BayeJxFW2SP4XRrFd1JYnxeLeU8EqN3vDfZmbqBqaGJAyiLjTAwm6ZLRQUMv1ZACTj37sR62cfN7fe5JnJ7dh8zL4fiyLHV"
     = master_key
-    |> KeyPair.derive("m/0'/1/2'/2") |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0'/1/2'/2") |> KeyPair.format_key()
     assert "xprvA2JDeKCSNNZky6uBCviVfJSKyQ1mDYahRjijr5idH2WwLsEd4Hsb2Tyh8RfQMuPh7f7RtyzTtdrbdqqsunu5Mm3wDvUAKRHSC34sJ7in334"
     = master_key |> KeyPair.derive("m/0'/1/2'/2") |> KeyPair.format_key()
 
@@ -49,11 +49,11 @@ defmodule ChildKeyDerivation do
     ## Chain m/0h/1/2h/2/1000000000
     assert "xpub6H1LXWLaKsWFhvm6RVpEL9P4KfRZSW7abD2ttkWP3SSQvnyA8FSVqNTEcYFgJS2UaFcxupHiYkro49S8yGasTvXEYBVPamhGW6cFJodrTHy"
     = master_key
-    |> KeyPair.derive("m/0'/1/2'/2/1000000000")
-    |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
-    assert "xprvA41z7zogVVwxVSgdKUHDy1SKmdb533PjDz7J6N6mV6uS3ze1ai8FHa8kmHScGpWmj4WggLyQjgPie1rFSruoUihUZREPSL39UNdE3BBDu76"
-    = master_key |> KeyPair.derive("m/0'/1/2'/2/1000000000") |> KeyPair.format_key()
-  end
+    |> KeyPair.derive("M/0'/1/2'/2/1000000000")
+     |> KeyPair.format_key()
+     assert "xprvA41z7zogVVwxVSgdKUHDy1SKmdb533PjDz7J6N6mV6uS3ze1ai8FHa8kmHScGpWmj4WggLyQjgPie1rFSruoUihUZREPSL39UNdE3BBDu76"
+     = master_key |> KeyPair.derive("m/0'/1/2'/2/1000000000") |> KeyPair.format_key()
+   end
 
 
 
@@ -66,7 +66,7 @@ defmodule ChildKeyDerivation do
 
     ## Chain m
     assert "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB"
-    = master_key |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    = master_key |> KeyPair.to_public_key() |> KeyPair.format_key()
     assert "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U"
     = KeyPair.format_key(master_key)
 
@@ -74,7 +74,8 @@ defmodule ChildKeyDerivation do
     ## Chain m/0
     assert "xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH"
     = master_key
-    |> KeyPair.derive("m/0") |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0")
+    |> KeyPair.format_key()
     assert "xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt"
     = master_key |> KeyPair.derive("m/0") |> KeyPair.format_key()
 
@@ -82,8 +83,8 @@ defmodule ChildKeyDerivation do
     ## Chain m/0/2147483647h
     assert "xpub6ASAVgeehLbnwdqV6UKMHVzgqAG8Gr6riv3Fxxpj8ksbH9ebxaEyBLZ85ySDhKiLDBrQSARLq1uNRts8RuJiHjaDMBU4Zn9h8LZNnBC5y4a"
     = master_key
-    |> KeyPair.derive("m/0/2147483647'")
-    |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0/2147483647'")
+    |> KeyPair.format_key()
     assert "xprv9wSp6B7kry3Vj9m1zSnLvN3xH8RdsPP1Mh7fAaR7aRLcQMKTR2vidYEeEg2mUCTAwCd6vnxVrcjfy2kRgVsFawNzmjuHc2YmYRmagcEPdU9"
     = master_key |> KeyPair.derive("m/0/2147483647'") |> KeyPair.format_key()
 
@@ -91,8 +92,8 @@ defmodule ChildKeyDerivation do
     ## Chain m/0/2147483647h/1
     assert "xpub6DF8uhdarytz3FWdA8TvFSvvAh8dP3283MY7p2V4SeE2wyWmG5mg5EwVvmdMVCQcoNJxGoWaU9DCWh89LojfZ537wTfunKau47EL2dhHKon"
     = master_key
-    |> KeyPair.derive("m/0/2147483647'/1")
-    |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0/2147483647'/1")
+    |> KeyPair.format_key()
     assert "xprv9zFnWC6h2cLgpmSA46vutJzBcfJ8yaJGg8cX1e5StJh45BBciYTRXSd25UEPVuesF9yog62tGAQtHjXajPPdbRCHuWS6T8XA2ECKADdw4Ef"
     = master_key |> KeyPair.derive("m/0/2147483647'/1") |> KeyPair.format_key()
 
@@ -100,8 +101,8 @@ defmodule ChildKeyDerivation do
     ## Chain m/0/2147483647h/1/2147483646h
     assert "xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL"
     = master_key
-    |> KeyPair.derive("m/0/2147483647'/1/2147483646'")
-    |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0/2147483647'/1/2147483646'")
+    |> KeyPair.format_key()
     assert "xprvA1RpRA33e1JQ7ifknakTFpgNXPmW2YvmhqLQYMmrj4xJXXWYpDPS3xz7iAxn8L39njGVyuoseXzU6rcxFLJ8HFsTjSyQbLYnMpCqE2VbFWc"
     = master_key |> KeyPair.derive("m/0/2147483647'/1/2147483646'") |> KeyPair.format_key()
 
@@ -109,11 +110,31 @@ defmodule ChildKeyDerivation do
     ## Chain m/0/2147483647h/1/2147483646h/2
     assert "xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt"
     = master_key
-    |> KeyPair.derive("m/0/2147483647'/1/2147483646'/2")
-    |> KeyPair.generate_master_key(:public) |> KeyPair.format_key()
+    |> KeyPair.derive("M/0/2147483647'/1/2147483646'/2")
+    |> KeyPair.format_key()
     assert "xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j"
     = master_key |> KeyPair.derive("m/0/2147483647'/1/2147483646'/2") |> KeyPair.format_key()
   end
+
+
+  test "error derivation" do
+    seed = "000102030405060708090a0b0c0d0e0f" |> Base.decode16!(case: :mixed)
+    master_key = KeyPair.generate_master_key(seed, :seed)
+
+    public_master_key =
+      master_key
+      |> KeyPair.derive("M/0'/1/2'")
+
+    assert_raise(
+      RuntimeError,
+      "Cannot derive Public Hardened child",
+      fn ->
+        KeyPair.derive(public_master_key, "M/100'")
+      end)
+
+  end
+
+
 
   ## Not working!!!
   #test "public_derivation" do
