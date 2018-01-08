0336e0a750f6123d332719ca9610b819408b9e9c
gspasov <georgi.spasov@quanterall.com>
2018-01-08 10:22:21 +0200

    [GH-#79] Fix naming of mix file module and app name

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule ElixirWallet.Mixfile do
+defmodule Aewallet.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :elixir_wallet,
+      app: :aewallet,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
