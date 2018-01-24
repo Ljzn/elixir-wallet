bb9c5553f18d03bd4e68bafa100fe8a0f894416c
gspasov <georgi.spasov@quanterall.com>
2018-01-24 15:02:32 +0200

    [GH-#89] Made a module with functionality for encrypting and decrypting data with public/private key

---
+++ lib/aewallet/contract.ex
@@ -0,0 +1,107 @@
+defmodule Aewallet.Contract do
+  @moduledoc """
+  This module is responsible for the functionality needed
+  for the SignContract app, Encryption and Decryption of a contract file
+  as well as signing the contract itself
+  """
+
+  alias Aewallet.Cypher, as: Cypher
+
+  @typedoc ""
+  @type option :: :encrypt | :decrypt
+
+  @doc """
+  Responsible for encrypting a contract file and saving the
+  encrypted file to a specified location.
+  """
+  @spec encrypt(String.t(), binary()) :: tuple()
+  def encrypt(file_path, pub_key) do
+    read_file(File.read(file_path), pub_key, file_path, :encrypt)
+  end
+
+  @doc """
+  Responsible for decrypting a contract file with the
+  corresponding private keyand and saving the decrypted version
+  in a specified location with a specified extension.
+  """
+  @spec decrypt(String.t(), binary()) :: tuple()
+  def decrypt(file_path, priv_key) do
+    read_file(File.read(file_path), priv_key, file_path, :decrypt)
+  end
+
+  ## Private functions.
+
+  @spec read_file(tuple(), binary(), String.t(), option()) :: tuple()
+  defp read_file({:ok, contract_data}, pub_key, file_path, :encrypt) do
+    do_encrypt(contract_data, pub_key, file_path)
+  end
+  defp read_file({:ok, contract_data}, priv_key, file_path, :decrypt) do
+    do_decrypt(contract_data, priv_key, file_path)
+  end
+
+  @spec read_file(tuple(), binary(), String.t(), option()) :: tuple()
+  defp read_file({:error, reason}, _key, file_path, _option) do
+    case reason do
+      :enoent ->
+        {:error, "The file at #{file_path} does not exist."}
+      :eaccess ->
+        {:error, "Missing permision for reading the #{file_path} file,
+        or for searching one of the parent directories."}
+      :eisdir ->
+        {:error, "The named file - #{file_path} is a directory."}
+      :enotdir ->
+        {:error, "A component of the file - #{file_path} name is not a directory."}
+      :enomem ->
+        {:error, "There is not enough memory for the contents of the file."}
+    end
+  end
+
+  @spec save_file(binary(), String.t(), option()) :: tuple()
+  defp save_file(data, file_path, option) do
+    file_name =
+      case option do
+        :encrypt ->
+          4
+          |> :crypto.strong_rand_bytes()
+          |> Base.encode16()
+
+        ## TODO: Make the name of the file more specific
+        :decrypt ->
+          "Contract"
+      end
+
+    new_path =
+      file_path
+      |> String.split("/")
+      |> Enum.drop(-1)
+      |> Enum.join("/")
+      |> Kernel.<>("/")
+      |> Kernel.<>(file_name)
+      |> Kernel.<>(".pdf")
+
+    {File.write!(new_path, data), new_path}
+  end
+
+  @spec do_encrypt(String.t(), binary(), String.t()) :: tuple()
+  defp do_encrypt(contract_data, pub_key, file_path) do
+    rand = :crypto.strong_rand_bytes(32)
+
+    {:ok, encryptor} = :libsecp256k1.ec_pubkey_tweak_mul(pub_key, rand)
+    {helper_key, _} = :crypto.generate_key(:ecdh, :secp256k1, rand)
+
+    encrypted_contract = Cypher.encrypt(contract_data, encryptor)
+
+    save_file(helper_key <> encrypted_contract, file_path, :encrypt)
+  end
+
+  @spec do_decrypt(binary(), binary(), String.t()) :: tuple()
+  defp do_decrypt(encrypted_data, priv_key, file_path) do
+    <<helper_key::binary-65, contract_data::binary>> = encrypted_data
+
+    {:ok, decryptor} = :libsecp256k1.ec_pubkey_tweak_mul(helper_key, priv_key)
+    decrypted_contract = Cypher.decrypt(contract_data, decryptor)
+
+    save_file(decrypted_contract, file_path, :decrypt)
+  end
+
+end
